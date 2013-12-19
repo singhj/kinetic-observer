3979b5d102faf15e99edc94cf14aef0e28f145d0
singhj <j.singh@earlystageit.com>
2013-12-19 10:04:09 -0500

    Added session_id support.

--- README.md
+++ README.md
@@ -1,38 +1,20 @@
 kinetic-observer
 ================
 
-Observation API for App Kinetics
---------------------------------
-
-The API is a REST API for applications to send data. It supports these calls:
-
- 1. POST `/api/1/databases/app-key/collections/data-type/` and
- 2. GET `/api/1/databases/app-key/collections/data-type/collect/`
-
-In the above, `app-key` and `data-type` are setup parameters. They are provided to the application owner when they sign up. They need to be kept secure. You will need separate keys for each of your instances, for example *development*, *QA*, *production*, etc.
-
-Notes:
- 1. The trailing slashes on the URLs have an effect on performance. You want them there.
- 2. The requests should be sent to `app-collect.earlystageit.com` using `http` or `https`.
- 3. Data from a browser should be sent using the GET interface. POST interface is also supported and more secure but make sure you test it with IE. It does work for all other browsers.
- 4. Data from a server should be sent using the POST interface. `https` isn't always available when originating from the server and for extra security, a method of signing POST requests is available. Ask us about details.
- 5. The API calls should be protected inside try/catch blocks so errors in the API don't affect your application's operation.
-
-API Usage Strategy
+Getting Started
 ------------------
 
-Review your application architecture for the type of data that needs to be collected. Typically Ajax calls are a good place to instrument.
+An example file `app-kinetics.js` is available in the repository. **Include it in your project but 
+not before revising three Javascript functions to adapt to your application.** 
+Sample implementations of the functions are provided.
 
-The instructions below apply for Ajax-oriented applications. For other types of applications, for example those that as based on page loads, collectors are available &mdash; contact us for those. If there is a need to collect server-based data, PHP-based collectors are also available.
+ 1. `get_site_key` parses the URL to decide the application instance being served - we can provide the keys
+ 1. `ak_get_user_id` obtains the user ID from the page. Usually the user ID is displayed on a page somewhere.
+ 1. `ak_get_session_id` obtains the session ID. It is usually available in a cookie. 
+ Session ID's are important for tracking if, for example, the same user ID is being used by multiple users.
+ This function is optional. If not needed, it can be replaced by a single line `return 'SessionCookieNotImplemented';`.
 
-
-How to use the API
-------------------
-
-An example file `app-kinetics.js` is available in the repository. **Include it in your project but first replace your-application-key and data-type fields** provided by us.
-Replace the `ak_get_user_id` function body with one that returns the user ID for your application.
-
-To monitor page loads, include these lines in your HTML files, immediately before or after your analytics snippet. 
+To monitor page loads using App Kinetics, include these lines in your HTML files, immediately before or after your analytics snippet. 
 
     <script type="text/javascript">
       if(typeof appKineticsPageLoad == 'function') {
--- app-kinetics.js
+++ app-kinetics.js
@@ -1,86 +1,131 @@
-var ak_xhr_url = "https://app-collect.earlystageit.com/api/1/databases/your-application-key/collections/data-type/";
+function get_site_key(){
+    var ak_site_keys = {
+      'app': 'your-production-key',
+      'qa':  'your-qa-key',
+      'dev': 'your-dev-key'
+    };
+    var host = window.location.host;
+    var subdomain = host.split('.')[0];
+    if (subdomain == 'app') {
+        return ak_site_keys['app'];
+    }
+    if (subdomain.startsWith('eng')) {
+        return ak_site_keys['qa'];
+    }
+    return ak_site_keys['dev'];
+}
 
 function ak_get_user_id() {
-	var userField = jQuery('a#signout.btn').html();
-	var userRE = /(Sign out+\s*)(\S*)/;
-	var result = userField.match(userRE);
-	var user_id = result[2];
-	return user_id;
+    var userField = jQuery('#above-container #welcome').html();
+    var welcomeExp = /([\S,]+\s*)([^<]*)(<.*)/;
+    var result = userField.match(welcomeExp);
+    var user_id = result[2];
+    return user_id;
+};
+
+function ak_get_session_id() {
+        /* Get session id from session cookie */
+    var all = document.cookie;  // Get all cookies in one big string
+    if (all === "")             // If the property is the empty string
+        return 'SessionCookieNotFound';
+    var list = all.split("; "); // Split into individual name=value pairs
+    for(var i = 0; i < list.length; i++) {  // For each cookie
+        var cookie = list[i];
+        var p = cookie.indexOf("=");        // Find the first = sign
+        var name = cookie.substring(0,p);   // Get cookie name
+        if (! name.startsWith('SESS') ) {continue; }
+        if (name == 'SESSc0e8b364e63408cdfa06138e6146f9c0') {continue; }
+        var value = cookie.substring(p+1);  // Get cookie value
+        value = decodeURIComponent(value);  // Decode the value
+        return value;
+    }
+    return 'SessionCookieNotImplemented';
 };
 
 /* Do not change anything below this line */
-
 Date.prototype.toISOString = Date.prototype.toISOString || function () {
-	function pad(n){return (n<10 ? '0'+n : n); };
-	return this.getUTCFullYear()+'-'
-		+ pad(this.getUTCMonth()+1)+'-'
-		+ pad(this.getUTCDate())+'T'
-		+ pad(this.getUTCHours())+':'
-		+ pad(this.getUTCMinutes())+':'
-		+ pad(this.getUTCSeconds())+'Z';
+    function pad(n){return n<10 ? '0'+n : n}
+    return this.getUTCFullYear()+'-'
+          + pad(this.getUTCMonth()+1)+'-'
+          + pad(this.getUTCDate())+'T'
+          + pad(this.getUTCHours())+':'
+          + pad(this.getUTCMinutes())+':'
+          + pad(this.getUTCSeconds())+'Z';
 };
 
 function appKineticsPageLoad() {
-	var startT = new Date();
-	try {
-		var url_with_qs = window.location.href;
-		var url = url_with_qs.split('?');
-		var userid = ak_get_user_id();
-		if (userid.length == 0) return;
-		var args = ((url.length > 1) ? url[1].split('&') : '');
-		var msg = {
-			'location': document.location.host,
-				'function': url[0], 
-				'arguments':args, 
-				'startT': startT.toISOString().substr(0,19), 
-				'agent': navigator.userAgent,
-				'user': userid
-		};
+    var startT = new Date();
+    try {
+        var url_with_qs = window.location.href;
+        var url = url_with_qs.split('?');
+        var userid = ak_get_user_id();
+        if (userid.length == 0) return;
+        var sessionid = ak_get_session_id();
+        var args = ((url.length > 1) ? url[1].split('&') : '');
+        var msg = {
+            'location': document.location.host,
+            'function': url[0], 
+            'arguments':args, 
+            'startT': startT.toISOString().substr(0,19), 
+            'agent': navigator.userAgent,
+            'user': userid,
+            'session': sessionid
+        };
 
-		var payload = {
-			'source': 'browser', 
-			'timestamp': startT.toISOString().substr(0,19), 
-			'message': JSON.stringify(msg)
-		};
-		var queryString = jQuery.param(payload);
-		var html = '<img src="'+ak_xhr_url+'collect/?'+queryString+'" alt="ak" border="0" width="1" height="1">';
-		jQuery('body').append(html);
-	}
-	finally {
-		return ;
-	}
+        var payload = {
+            'source': 'browser', 
+            'timestamp': startT.toISOString().substr(0,19), 
+            'message': JSON.stringify(msg)
+        };
+        var queryString = jQuery.param(payload);
+        var html = '<img src="'+ak_xhr_url+'collect/?'+queryString+'" alt="ak" border="0" width="1" height="1">';
+        jQuery('body').append(html);
+    }
+    finally {
+        return ;
+    }
 }
 
 function appKineticsAjaxDecorator(f) {
-	return function() {
-		var startT = new Date();
-		var answer = f.apply(this, arguments);
-		try {
-			var deltaT = ((new Date()).getTime() - startT.getTime())/1000.0;
-			var url_with_qs = (arguments["0"].hasOwnProperty("url") ? arguments["0"]["url"] : arguments["0"]);
-			var url = url_with_qs.split('?');
-			var userid = ak_get_user_id();
-			var args = (url.length > 1 ? url[1].split('&') : '');
-			var msg = {
-				'location': document.location.host,
-				'function': url[0], 
-				'arguments':args, 
-				'startT': startT.toISOString().substr(0,19), 
-				'deltaT': deltaT,
-					'agent': navigator.userAgent,
-					'user': userid
-				};
-				var payload = {
-					'source': 'browser', 
-					'timestamp': startT.toISOString().substr(0,19), 
-					'message': JSON.stringify(msg)
-				};
-				var queryString = jQuery.param(payload);
-				var html = '<img src="'+ak_xhr_url+'collect/?'+queryString+'" alt="ak" border="0" width="1" height="1">';
-				jQuery('body').append(html);
-		}
-		finally {
-				return answer;
-		}
-	};
+  return function() {
+    var startT = new Date();
+    var answer = f.apply(this, arguments);
+    try {
+        var deltaT = ((new Date()).getTime() - startT.getTime())/1000.0;
+        var url_with_qs = (arguments["0"].hasOwnProperty("url") ? arguments["0"]["url"] : arguments["0"]);
+        var url = url_with_qs.split('?');
+        var userid = ak_get_user_id();
+        var sessionid = ak_get_session_id();
+        var args = (url.length > 1 ? url[1].split('&') : '');
+        var msg = {
+            'location': document.location.host,
+            'function': url[0],
+            'arguments':args,
+            'startT': startT.toISOString().substr(0,19),
+            'deltaT': deltaT,
+            'agent': navigator.userAgent,
+            'user': userid,
+            'session': sessionid
+        };
+
+        var payload = {
+            'source': 'browser',
+            'timestamp': startT.toISOString().substr(0,19),
+            'message': JSON.stringify(msg)
+        };
+        var queryString = jQuery.param(payload);
+        var ak_xhr_url = 'https://app-collect.earlystageit.com/api/1/databases/'+get_site_key()+'/collections/main/';
+        var html = '<img src="'+ak_xhr_url+'collect/?'+queryString+'" alt="ak" border="0" width="1" height="1">';
+        jQuery('body').append(html);
+    }
+    finally {
+        return answer;
+    }
+  }
+}
+
+if (typeof String.prototype.startsWith != 'function') {
+    String.prototype.startsWith = function (str){
+        return this.slice(0, str.length) == str;
+    };
 }
